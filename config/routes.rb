de72dcb01b24bafd304125b64d7646a956f00f01
Eliott Mogenet <eliott.mogenet@gmail.com>
2020-08-17 16:56:27 +0200

    create routes for droids

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :droids
+
+  #resources :users do
+    #resources :droids do
+      #resources :rents, only: [:index,:show, :new, :create, :destroy]
+    #end
+  #end
+
+  end
