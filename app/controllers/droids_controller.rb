d0c26d0d6aebfc0e00f27051592bb5f1262be09c
Adrien Fort <adrien.fort01@gmail.com>
2020-08-17 17:57:11 +0200

    add the edit and update methods to the droid controller

--- app/controllers/droids_controller.rb
+++ app/controllers/droids_controller.rb
@@ -4,5 +4,9 @@ class DroidsController < ApplicationController
   end
 
   def update
+    @droid = Droid.new(params_droid)
+    @droid.user = current_user
+    @droid.save
+    redirect_to droids_path
   end
 end
