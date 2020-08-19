3885fab82bac52129917ede3585c0b03706f75c8
Eliott Mogenet <eliott.mogenet@gmail.com>
2020-08-19 11:20:07 +0200

    save

--- app/models/droid.rb
+++ app/models/droid.rb
@@ -4,7 +4,7 @@ class Droid < ApplicationRecord
   has_one_attached :photo
   validates :name, :category, :builder, :description, :era, :price_per_year, presence: true
   validates :name, uniqueness: true
-  ERA = ['ljhglhj', 'mkjhgmkugh', 'omughmug']
-  CATEGORY = ['mkjhmkjh', 'mkjhgmkugh', 'omughmug']
-  BUILDER = ['mkjhmkjh', 'mkjhgmkugh', 'omughmug']
+  ERA =  ['Old Republic', 'New Republic', 'Empire Sith' ]
+  CATEGORY = ['Astomecano', 'Combat', 'Medical', 'Technicien', 'Utilitaire' ]
+  BUILDER = ['Les mines de Dorvalla', 'Maison organa', 'République galactique', 'Résistance']
 end
