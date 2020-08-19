520b7ed256f9532a5a8ac319e047364f5f2fb0ac
nicolas chevalier <nicolas_chevalier08@hotmail.fr>
2020-08-19 15:43:17 +0200

    show almost done

--- app/models/droid.rb
+++ app/models/droid.rb
@@ -4,7 +4,7 @@ class Droid < ApplicationRecord
   has_one_attached :photo
   validates :name, :category, :builder, :description, :era, :price_per_year, presence: true
   validates :name, uniqueness: true
-  ERA =  ['Old Republic', 'New Republic', 'Empire Sith' ]
+  ERA = ['Old Republic', 'New Republic', 'Empire Sith' ]
   CATEGORY = ['Astomecano', 'Combat', 'Medical', 'Technicien', 'Utilitaire' ]
   BUILDER = ['Les mines de Dorvalla', 'Maison organa', 'République galactique', 'Résistance']
 end
