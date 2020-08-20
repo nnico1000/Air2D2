c5c3ebd934d69ba0122f216f5a0bd199281ff112
Adrien Fort <adrien.fort01@gmail.com>
2020-08-20 19:41:59 +0200

    seed not ready, forgot to do the rest

--- app/models/droid.rb
+++ app/models/droid.rb
@@ -7,13 +7,20 @@ class Droid < ApplicationRecord
   ERA = ['Old Republic', 'New Republic', 'Empire Sith' ]
   CATEGORY = ['Astomecano', 'Combat', 'Medical', 'Technicien', 'Utilitaire' ]
   BUILDER = ['Les mines de Dorvalla', 'Maison organa', 'République galactique', 'Résistance']
+  PICTURE = ["https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/2vr8_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droide_arbitre_he2_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droiderqvig.jpg",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droide_dx11_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/t0b4_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/bf4f_vignette.png",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droideka.jpg", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/yg3_vignette.png",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/triple0_vig.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/o-66_vig.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/c3po_vignette.jpg",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/R3DO_avatar.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/zero_vignette.png",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/z9v86_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/tyth_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/e10_vignette.jpg",
+  "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/224ava.jpg", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/2BB2vignette.jpg", "https://i.pinimg.com/originals/fa/16/42/fa164207f82b769dec7719b3156355e3.jpg"]
 
 
   def booked?(start_year, end_year)
     Rent.all.each do |r|
       return false if (r.start_year..r.end_year).overlaps?(start_year..end_year)
     end
-    #rents.where("start_year < ?", start_year).where("end_year > ?", start_year).first.present? || rents.where("start_year < ?", end_year).where("end_year > ?", end_year).first.present? || rents.where("start_year > ?", start_year).where("end_year < ?", end_year).first.present?
+    # rents.where("start_year < ?", start_year).where("end_year > ?", start_year).first.present? || rents.where("start_year < ?", end_year).where("end_year > ?", end_year).first.present? || rents.where("start_year > ?", start_year).where("end_year < ?", end_year).first.present?
   end
 
   include PgSearch::Model
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'open-uri'
+
 Rent.destroy_all
 Droid.destroy_all
 User.destroy_all
+
+PICTURE = ["https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/2vr8_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droide_arbitre_he2_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droiderqvig.jpg",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droide_dx11_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/t0b4_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/bf4f_vignette.png",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/droideka.jpg", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/yg3_vignette.png",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/triple0_vig.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/o-66_vig.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/c3po_vignette.jpg",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/R3DO_avatar.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/zero_vignette.png",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/z9v86_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/tyth_vignette.png", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/e10_vignette.jpg",
+"https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/224ava.jpg", "https://www.starwars-universe.com/images/encyclopedie/droides/vignettes_v6/2BB2vignette.jpg", "https://i.pinimg.com/originals/fa/16/42/fa164207f82b769dec7719b3156355e3.jpg"]
+
 
 # hans = User.create!(name: "Hans", email: "hans@gmail.com", password: "123456")
 # hans_2 = User.create!(name: "Hans2", email: "hans2@gmail.com", password: "123456")
@@ -32,7 +43,11 @@ puts "Creating 50 users, droids and rents"
 
 50.times do
   user = User.new(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, password: ('a'..'z').to_a.sample(8).join, species: User::SPECIES.sample, planet: User::PLANET.sample, radio_frequency: rand(10000.0...100000.0))
+  # user.photo = Droid::PICTURE.sample
   user.save
+  # uri = URI.parse(Droid::PICTURE.sample)
+  # file = URI.open(Droid::PICTURE.sample)
+  # user.photo.attach(io: uri.read, filename: 'photo_droid.png')
   droid = Droid.new(name: Faker::Name.unique.first_name, category: Droid::CATEGORY.sample, builder: Droid::BUILDER.sample, age: (50..2000).to_a.sample, description: Faker::Lorem.paragraph(sentence_count: 3), price_per_year: (1000..10000).to_a.sample, era: Droid::ERA.sample)
   droid.user = user
   droid.save
