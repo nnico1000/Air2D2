8e65b6e831ec3125045354e2604ae752b19df9c8
Adrien Fort <adrien.fort01@gmail.com>
2020-08-21 15:28:09 +0200

    we are ready

--- app/javascript/components/jauge.js
+++ app/javascript/components/jauge.js
@@ -23,61 +23,11 @@ const jauge = () => {
       document.getElementById("years").textContent = sliderValue;
     };
 
-  // if (document.getElementById('ex1')) {
-  //   var slider = new Slider('#ex1', {
-  //   formatter: function(value) {
-  //   return 'Current value: ' + value;
-  //     }
-  //   });
-
-  //   let target = document.getElementById('ex1Slider');
-
-  //   let target = document.getElementById('ex1Slider');
-
-  //   if (target) {
-  //   const calcul = () => {
-  //     let sliderValue = target.querySelector('.slider-handle').getAttribute('aria-valuenow')
-  //     let price = document.getElementById('ex1').dataset.price
-
-  //     let totalPrice = sliderValue * price
-  //     console.log('slideer value');
-  //     console.log(sliderValue);
-  //     console.log('price_per_year value');
-
-  //     console.log(totalPrice);
-  //     document.getElementById("calculprice").textContent = totalPrice;
-  //     document.getElementById("years").textContent = sliderValue;
-  //   }
-  //     slider.on('slide',function(sliderValue) {
-
-  //     console.log(totalPrice);
-  //     document.getElementById("calculprice").textContent = totalPrice;
-  //     document.getElementById("years").textContent = sliderValue;
-
-  //   }
-  //     slider.on('slide',function(sliderValue) {
-
-
-  //   let totalPrice = sliderValue * price
-  //   // console.log('slideer value');
-  //   // console.log(sliderValue);
-  //   // console.log('price_per_year value');
-
-  //   // console.log(totalPrice);
-  //   document.getElementById("calculprice").textContent = totalPrice;
-  //   document.getElementById("years").textContent = sliderValue;
-  //       calcul();
-  //       // document.getElementById("ex6SliderVal").textContent = sliderValue;
-  //       });
-  //   }
-
     slider.on('slide',function(sliderValue) {
         calcul();
       // document.getElementById("ex6SliderVal").textContent = sliderValue;
       });
   }
-
-}
 
 }
 
